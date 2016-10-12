78bd4cdd0bdee251539d11ae24d58c1dc882c25b
sumin <suminc7@naver.com>
2016-10-12 12:10:42 +0900

    video list

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 
 const API_KEY = 'AIzaSyDylqlMq6-y9m5Um6vpmoq2sr6Upwv4fCM';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
-};
+
+
+
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos: videos });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        )
+    }
+}
 
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
