76a23a624445a385bbbb522114e1af6f8ecc2bba
sumin <suminc7@naver.com>
2016-10-21 17:56:29 +0900

    reduxPromise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '3ecd8fd44b07ea411bdb6bfef166a91d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FEACH_WEATHER';
+
+export function fetchWeather(city){
+
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
