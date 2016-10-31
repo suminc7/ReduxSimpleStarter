3543fae429b0cd740bcbfe80eaede833b0093b4b
sumin <sumin@does.kr>
2016-10-31 18:30:10 +0900

    redux form

---
+++ src/components/posts_new.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+
+class PostsNew extends Component {
+    render() {
+
+        const { fields: {title, categories, content }, handleSubmit } = this.props;
+        console.log(title)
+
+        return (
+            <form onSubmit={handleSubmit(this.props.createPost)}>
+                <h3>Create A New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title} />
+                </div>
+
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories} />
+                </div>
+
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" {...content} />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+
+export default reduxForm({
+    form: 'PostNewForm',
+    fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 
 const Greeting = () => {
@@ -12,6 +13,6 @@ const Greeting = () => {
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={PostsIndex} />
-        <Route path="greet" component={Greeting} />
+        <Route path="posts/new" component={PostsNew} />
     </Route>
 )
